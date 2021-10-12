c47d251bdde3453e2ce7883b60a2c7e5d69c6ce7
Edwar Carranza <edwarcarranza@gmail.com>
2021-10-12 16:22:43 +1100

    It implements the new post functionality.  It was enhanced the navigation toolbar links and it was modified the feed view, showing database registers

--- users/views.py
+++ users/views.py
@@ -13,16 +13,37 @@ from django.db.utils import IntegrityError
 from django.contrib.auth.models import User
 from users.models import Profile
 
+# Forms
+from users.forms import ProfileForm
+
+@login_required
 def update_profile(request):
     """Update a user's profile view."""
     profile = request.user.profile
-    
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+            print(data)
+            return redirect('update_profile')
+
+    else:
+        form = ProfileForm()
+
     return render(
         request=request,
         template_name='users/update_profile.html',
         context={
             'profile': profile,
-            'user': request.user
+            'user': request.user,
+            'form': form
         }
     )
 
